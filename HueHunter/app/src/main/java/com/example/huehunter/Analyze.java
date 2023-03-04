d7ce359888a80d39d30e3c059a3b7401e9bc4e6e
JohnWick08 <mgao041@uottawa.ca>
2023-03-04 10:23:31 -0500

    update folder

--- HueHunter/app/src/main/java/com/example/huehunter/Analyze.java
+++ HueHunter/app/src/main/java/com/example/huehunter/Analyze.java
@@ -2,13 +2,24 @@ package com.example.huehunter;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Analyze extends AppCompatActivity {
     ImageView view;
@@ -18,19 +29,42 @@ public class Analyze extends AppCompatActivity {
         setContentView(R.layout.activity_analyze);
 
         view = (ImageView) this.findViewById(R.id.imageView);
+
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(intent,100);
     }
 
+
+
+    ///
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
 
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode==100){
             Bitmap bitmap = (Bitmap)data.getExtras().get("data");
-            if(null == view) {
-                Log.e("Error", "Ouh! there is no there is no child view with R.id.imageView ID within my parent view View.");
-            }
-            view.setImageBitmap(bitmap);
+            onCaptureImageResult(data);
         }
+    }
+    private void onCaptureImageResult(Intent data) {
+        Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        thumbnail.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+
+        File destination = new File(Environment.getExternalStorageDirectory(),
+                System.currentTimeMillis() + ".jpg");
+
+        FileOutputStream fo;
+        try {
+            destination.createNewFile();
+            fo = new FileOutputStream(destination);
+            fo.write(bytes.toByteArray());
+            fo.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        view.setImageBitmap(thumbnail);
     }
 }
