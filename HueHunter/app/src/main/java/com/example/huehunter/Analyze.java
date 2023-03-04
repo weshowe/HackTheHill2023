9146b03b8f1ac36db66b6cf7ddff9054343648a5
JohnWick08 <mgao041@uottawa.ca>
2023-03-04 10:39:10 -0500

    feature of taking high quality image

--- HueHunter/app/src/main/java/com/example/huehunter/Analyze.java
+++ HueHunter/app/src/main/java/com/example/huehunter/Analyze.java
@@ -6,6 +6,8 @@ import androidx.core.content.FileProvider;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -13,6 +15,8 @@ import android.provider.MediaStore;
 import android.util.Log;
 import android.widget.ImageView;
 import android.speech.tts.TextToSpeech;
+import android.widget.Toast;
+
 import java.util.Locale;
 
 import java.io.ByteArrayOutputStream;
@@ -25,59 +29,101 @@ import java.util.Date;
 
 public class Analyze extends AppCompatActivity {
     ImageView view;
+    Uri outPutfileUri;
+    String mCurrentPhotoPath;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_analyze);
 
-        ttsr=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
-         @Override
-         public void onInit(int status) {
-            if(status != TextToSpeech.ERROR) {
-               ttsr.setLanguage(Locale.US);
-            }
-         }
-        });
-
-        ttsr.speak("This is a test to see if the Android TTS works", TextToSpeech.QUEUE_FLUSH, null);
 
         view = (ImageView) this.findViewById(R.id.imageView);
 
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent,100);
+        Intent intent= new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File file = new File(Environment.getExternalStorageDirectory(),
+                "MyPhoto.jpg");
+        outPutfileUri = FileProvider.getUriForFile(this, this.getApplicationContext().getPackageName() + ".provider", createImageFile());
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, outPutfileUri);
+        startActivityForResult(intent, 1);
     }
+    Bitmap bitmap = null;
+
+    private File createImageFile()  {
+        // Create an image file name
+        try {
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            String imageFileName = "JPEG_" + timeStamp + "_";
+            File storageDir = new File(Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_DCIM), "Camera");
+            File image = File.createTempFile(
+                    imageFileName,  /* prefix */
+                    ".jpg",         /* suffix */
+                    storageDir      /* directory */
+            );
+
+            // Save a file: path for use with ACTION_VIEW intents
+            mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+            return image;
+        }
+        catch(Exception e){
+            return null;
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+
+            String uri = outPutfileUri.toString();
+            Log.e("uri-:", uri);
+            Toast.makeText(this, outPutfileUri.toString(), Toast.LENGTH_LONG).show();
+
+            //Bitmap myBitmap = BitmapFactory.decodeFile(uri);
+            // mImageView.setImageURI(Uri.parse(uri));   OR drawable make image strechable so try bleow also
+
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), outPutfileUri);
+                Drawable d = new BitmapDrawable(getResources(), bitmap);
+                view.setImageDrawable(d);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+    }}
 
 
 
     ///
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==100){
-            Bitmap bitmap = (Bitmap)data.getExtras().get("data");
-            onCaptureImageResult(data);
-        }
-    }
-    private void onCaptureImageResult(Intent data) {
-        Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        thumbnail.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-
-        File destination = new File(Environment.getExternalStorageDirectory(),
-                System.currentTimeMillis() + ".jpg");
-
-        FileOutputStream fo;
-        try {
-            destination.createNewFile();
-            fo = new FileOutputStream(destination);
-            fo.write(bytes.toByteArray());
-            fo.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        view.setImageBitmap(thumbnail);
-    }
-}
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if(requestCode==100){
+//            Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+//            onCaptureImageResult(data);
+//        }
+//    }
+//    private void onCaptureImageResult(Intent data) {
+//        Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+//        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+//        thumbnail.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+//
+//        File destination = new File(Environment.getExternalStorageDirectory(),
+//                System.currentTimeMillis() + ".jpg");
+//
+//        FileOutputStream fo;
+//        try {
+//            destination.createNewFile();
+//            fo = new FileOutputStream(destination);
+//            fo.write(bytes.toByteArray());
+//            fo.close();
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        view.setImageBitmap(thumbnail);
+//    }
+//}
