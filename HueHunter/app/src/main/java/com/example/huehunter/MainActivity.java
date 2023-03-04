ed4ab5617c3c9a87090aaaad35fa63e931e5f1e6
Marc WU <32423253+wumarc@users.noreply.github.com>
2023-03-04 01:43:15 -0500

    adding main screen

--- HueHunter/app/src/main/java/com/example/huehunter/MainActivity.java
+++ HueHunter/app/src/main/java/com/example/huehunter/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.huehunter;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void cameraClick(View view) {
+        Intent i = new Intent(getApplicationContext(),WakeUpSheet.class);
+        startActivity(i);
+    }
+
+
+
 }
