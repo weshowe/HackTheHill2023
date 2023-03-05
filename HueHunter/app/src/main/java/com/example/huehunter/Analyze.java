97c4b803e149e9e48bbff9349443d321d9d20203
Ethan Plant <plant.ethan@gmail.com>
2023-03-05 00:13:54 -0500

    Readd Color import

--- HueHunter/app/src/main/java/com/example/huehunter/Analyze.java
+++ HueHunter/app/src/main/java/com/example/huehunter/Analyze.java
@@ -5,6 +5,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
